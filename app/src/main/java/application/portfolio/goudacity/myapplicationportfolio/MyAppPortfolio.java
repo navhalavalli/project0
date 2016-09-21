5865166bc607256ad323d7a58e8d92bfa99700db
hegden <hegden@vmware.com>
2016-09-21 16:29:57 +0530

    First commit to check upsync.

---
+++ app/src/main/java/application/portfolio/goudacity/myapplicationportfolio/MyAppPortfolio.java
@@ -0,0 +1,13 @@
+package application.portfolio.goudacity.myapplicationportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyAppPortfolio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_app_portfolio);
+    }
+}
